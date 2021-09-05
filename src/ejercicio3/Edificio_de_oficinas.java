3b59891a9d5150ee3a0af49d898effef60de9140
Matias Flori <matyflori@hotmail.com>
2021-09-04 21:29:45 -0300

    V 0.4
    Realice la carga del ejercicio 2 e hice la interfaz de IEdificio como acordamos entre todos, también agregue la clase edificio_de_oficinas implementando dicha interfaz poniendo todo lo que indica el ejercicio 3

--- src/ejercicio3/Edificio_de_oficinas.java
+++ src/ejercicio3/Edificio_de_oficinas.java
@@ -1,5 +1,43 @@
 package ejercicio3;
 
-public class Edificio_de_oficinas {
+public class Edificio_de_oficinas implements IEdificio {
 
+	private int cant_oficinas;
+	private double SuperficieEdificio;
+	
+	Edificio_de_oficinas(){
+		
+	}
+
+	Edificio_de_oficinas(double SuperficieEdificio, int cant_oficinas){
+		
+		this.cant_oficinas = cant_oficinas;
+		this.SuperficieEdificio= SuperficieEdificio;
+	}
+	
+	public int getCant_oficinas() {
+		return cant_oficinas;
+	}
+
+	public void setCant_oficinas(int cant_oficinas) {
+		this.cant_oficinas = cant_oficinas;
+	}
+
+	@Override
+	public String toString() {
+		return "Superficie del edificio: " + getSuperficieEdificio() +". Cantidad de oficinas: " + this.cant_oficinas ;
+	}
+
+	@Override
+	public double getSuperficieEdificio() {
+		return this.SuperficieEdificio;
+	}
+
+	@Override
+	public void setSuperficieEdificio(double SuperficieEdificio) {
+		this.SuperficieEdificio= SuperficieEdificio;
+		
+	}
+	
+	
 }
--- src/ejercicio3/IEdificio.java
+++ src/ejercicio3/IEdificio.java
@@ -1,5 +1,7 @@
 package ejercicio3;
 
 public interface IEdificio {
-
+	
+	double getSuperficieEdificio();
+	void setSuperficieEdificio(double SuperficieEdificio);
 }
